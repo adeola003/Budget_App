f11793abd6eed2c32454b9c2076b68b32382d767
adeola003 <khadaf3@gmail.com>
2023-08-06 16:13:56 +0100

    run migration and create tables in db

--- db/migrate/20230805193611_create_entities.rb
+++ db/migrate/20230805193611_create_entities.rb
@@ -1,6 +1,10 @@
 class CreateEntities < ActiveRecord::Migration[7.0]
   def change
     create_table :entities do |t|
+      t.string :name
+      t.decimal :amount, precision: 10, scale: 2
+      t.references :user, null: false, foreign_key: true
+      t.references :group, null: false, foreign_key: true
 
       t.timestamps
     end
