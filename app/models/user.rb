bd388115252c6d7614b4291d1da72b8fc81b231b
adeola003 <khadaf3@gmail.com>
2023-08-06 17:11:01 +0100

    install devise and create views

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+  
   has_many :groups, dependent: :destroy
   has_many :entities, through: :groups
 
