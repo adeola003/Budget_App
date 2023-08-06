98604b52d84d613e5fa6abc9317914307a14e62b
adeola003 <khadaf3@gmail.com>
2023-08-06 18:13:06 +0100

    update route for registration

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'registrations/registrations' }
 end
