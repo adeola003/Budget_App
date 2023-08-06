55bef4f30e00689c98e5571a9474fbdea3e90292
adeola003 <khadaf3@gmail.com>
2023-08-06 17:04:45 +0100

    create models and run migration

---
+++ app/models/entity.rb
@@ -0,0 +1,14 @@
+class Entity < ApplicationRecord
+  after_initialize :init
+
+  belongs_to :user
+  belongs_to :group
+
+  validates :name, :amount, presence: true
+  validates :amount, numericality: { greater_than: 0 }
+
+  def init
+    self.amount ||= 1
+  end
+
+end
