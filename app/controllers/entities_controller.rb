f092057dfcbb04f717468b794b5e92d0bae1339b
adeola003 <khadaf3@gmail.com>
2023-08-07 14:22:27 +0100

    add methods in the controllers

--- app/controllers/entities_controller.rb
+++ app/controllers/entities_controller.rb
@@ -1,2 +1,56 @@
 class EntitiesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_group
+  before_action :set_entity, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @entities = @group.entities.order(created_at: :desc)
+    @total_amount = @entities.sum(:amount)
+  end
+
+  def show
+  end
+
+  def new
+    @entity = @group.entities.build
+  end
+
+  def create
+    @entity = @group.entities.build(entity_params)
+    if @entity.save
+      redirect_to group_entities_path(@group), notice: "Entity created successfully."
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @entity.update(entity_params)
+      redirect_to group_entities_path(@group), notice: "Entity updated successfully."
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @entity.destroy
+    redirect_to group_entities_path(@group), notice: "Entity deleted successfully."
+  end
+
+  private
+
+  def set_group
+    @group = current_user.groups.find(params[:group_id])
+  end
+
+  def set_entity
+    @entity = @group.entities.find(params[:id])
+  end
+
+  def entity_params
+    params.require(:entity).permit(:name, :amount)
+  end
 end
--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,52 @@
 class GroupsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_group, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @groups = current_user.groups
+  end
+
+  def show
+    @entities = @group.entities.order(created_at: :desc)
+    @total_amount = @entities.sum(:amount)
+  end
+
+  def new
+    @group = current_user.groups.build
+  end
+
+  def create
+    @group = current_user.groups.build(group_params)
+    if @group.save
+      redirect_to group_path(@group), notice: "Group created successfully."
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @group.update(group_params)
+      redirect_to group_path(@group), notice: "Group updated successfully."
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @group.destroy
+    redirect_to groups_path, notice: "Group deleted successfully."
+  end
+
+  private
+
+  def set_group
+    @group = current_user.groups.find(params[:id])
+  end
+
+  def group_params
+    params.require(:group).permit(:name, :icon)
+  end
 end
