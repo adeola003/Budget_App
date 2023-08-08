43db38290d9bc68719fcc29861a2d4a8fa3b2a0f
adeola003 <khadaf3@gmail.com>
2023-08-08 10:47:28 +0100

    safe commit

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "groups#index"
-  devise_for :users, controllers: { registrations: 'registrations/registrations' }
+  devise_for :users, controllers: {
+    registrations: 'registrations/registrations',
+    sessions: 'users/sessions'
+  }
 
   resources :groups, only: [:index, :show, :new, :create, :edit, :update, :destroy] do
     resources :entities, only: [:index, :show, :new, :create, :edit, :update, :destroy]
